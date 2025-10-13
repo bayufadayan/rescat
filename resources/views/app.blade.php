ee648d0366f4800ffe83213636e119c52067936f
bayufadayan <bayufadayan@gmail.com>
2025-10-13 21:09:51 +0700

    chore: change favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/images/icon/favicon-white.svg" type="image/svg+xml">
+        <link rel="icon" href="/images/icon/favicon-new.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
