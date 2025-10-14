b3a8f98908ab16c2ba0d6e38c1afdb4bcf1561bb
bayufadayan <bayufadayan@gmail.com>
2025-10-14 23:46:57 +0700

    feat: create LastCheck file

--- resources/js/pages/main.tsx
+++ resources/js/pages/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppLayout from '@/layouts/app-layout';
 import HeroSection from '@/components/section/hero/hero';
+import LastCheck from '@/components/section/last-check/last-check';
 
 export default function MainPage() {
     return (
         <AppLayout>
             <HeroSection />
+            <LastCheck />
         </AppLayout>
     )
 }
