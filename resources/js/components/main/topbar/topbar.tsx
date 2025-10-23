6e7ab4382f424384b2d19cdf803190cecf189223
bayufadayan <bayufadayan@gmail.com>
2025-10-23 14:59:49 +0700

    style: add topbar backdrop-blur while scrolled

--- resources/js/components/main/topbar/topbar.tsx
+++ resources/js/components/main/topbar/topbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from '@inertiajs/react';
 import { useSidebar } from "@/contexts/SidebarContext";
 import { useRoute } from 'ziggy-js';
@@ -6,26 +6,44 @@ import { useRoute } from 'ziggy-js';
 export default function Topbar() {
     const route = useRoute();
     const { toggleSidebar } = useSidebar();
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            setScrolled(scrollPosition > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
 
     return (
-        <div className='flex flex-row justify-between w-full h-12 items-center'>
-            <button onClick={toggleSidebar} className="w-10 h-full">
-                <figure className='flex justify-center items-center'>
-                    <img src="/images/icon/sidebar-outline.svg" alt="ResCat" className="h-full w-auto object-contain" />
-                </figure>
-            </button>
+        <nav
+            className={`w-full fixed top-0 left-0 p-4 z-50 transition-all duration-1000 ease-in-out
+                ${scrolled ? 'backdrop-blur-md shadow-md' : 'bg-transparent shadow-none backdrop-blur-none'}
+            `}
+        >
+            <div className='flex flex-row justify-between w-full h-12 items-center'>
+                <button onClick={toggleSidebar} className="w-10 h-full">
+                    <figure className='flex justify-center items-center'>
+                        <img src="/images/icon/sidebar-outline.svg" alt="ResCat" className="h-full w-auto object-contain" />
+                    </figure>
+                </button>
 
-            <Link href={route('home')}>
-                <figure>
-                    <img src="/images/icon/logo-rescat.svg" alt="ResCat" className="h-full w-auto object-contain" />
-                </figure>
-            </Link>
+                <Link href={route('home')}>
+                    <figure>
+                        <img src="/images/icon/logo-rescat.svg" alt="ResCat" className="h-full w-auto object-contain" />
+                    </figure>
+                </Link>
 
-            <button className='w-10 h-full'>
-                <figure>
-                    <img src="/images/icon/premium-button.svg" alt="ResCat" className="h-full w-auto object-contain" />
-                </figure>
-            </button>
-        </div>
+                <button className='w-10 h-full'>
+                    <figure>
+                        <img src="/images/icon/premium-button.svg" alt="ResCat" className="h-full w-auto object-contain" />
+                    </figure>
+                </button>
+            </div>
+        </nav>
     )
 }
--- resources/js/layouts/app-layout.tsx
+++ resources/js/layouts/app-layout.tsx
@@ -6,10 +6,8 @@ import { SidebarProvider } from "@/contexts/SidebarContext";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
     return (
         <SidebarProvider>
-            <main className="min-h-svh h-full flex flex-col relative bg-[#EAF2F9]">
-                <nav className="w-full fixed p-4 z-50">
-                    <Topbar />
-                </nav>
+            <main className="min-h-dvh h-full flex flex-col relative bg-[#EAF2F9]">
+                <Topbar />
                 <div className='flex flex-col w-full gap-4'>{children}</div>
                 <Sidebar />
             </main>
